e7184c7f14f71b4fb0951460c492776393bc9cf4
Edison Ooi <edison.ooi@duke.edu>
2021-10-23 17:31:50 -0400

    Added slang translator project

---
+++ SlangTranslator/SlangTranslator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlangTranslator
+//
+//  Created by Edison Ooi on 10/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
