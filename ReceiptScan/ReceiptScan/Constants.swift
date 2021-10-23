a3ba4deb709a7f507ee33190fb95cf8e04d51b20
Edison Ooi <edison.ooi@duke.edu>
2021-10-23 15:55:04 -0400

    Added SwiftyJSON and AlamoFire

---
+++ ReceiptScan/ReceiptScan/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ReceiptScan
+//
+//  Created by Edison Ooi on 10/23/21.
+//
+
+import Foundation
+
+enum APIUrls: String {
+    case test = "test"
+}
--- ReceiptScan/ReceiptScan/LoginViewController.swift
+++ ReceiptScan/ReceiptScan/LoginViewController.swift
@@ -14,21 +14,47 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var newAccountButton: UIButton!
     
+    @IBOutlet weak var bruh: UIButton!
+    var username: String = ""
+    var password: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        guard let name = usernameTextField.text, let pass = passwordTextField.text else {return}
+        
+        if name == "" || pass == "" {
+            return
+        }
+        
+        //Show loading screen
+        let child = SpinnerViewController()
+        
+        addChild(child)
+        child.view.frame = view.frame
+        view.addSubview(child.view)
+        child.didMove(toParent: self)
+        
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            //Remove loading view upon completion of Hypixel API calls
+            child.willMove(toParent: nil)
+            child.view.removeFromSuperview()
+            child.removeFromParent()
+        })
+        
+        
+        
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        
     }
-    */
-
+    
 }
