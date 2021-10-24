d1d0ad79f20fcc6017988854d6c5d505298cc084
Havish Malladi <havishmalladi@gmail.com>
2021-10-24 10:03:40 -0400

    using google vision

--- ocr/main.py
+++ ocr/main.py
@@ -1,11 +1,10 @@
 import sys
 from flask import escape
-import pytesseract
-import cv2
 import boto3
 import numpy as np
 import os
 import json
+from google.cloud import vision
 
 def hello_get(request):
     s3 = boto3.resource(
@@ -15,28 +14,16 @@ def hello_get(request):
         aws_secret_access_key=os.getenv("SECRET_KEY")
     )
     bucket = s3.Bucket('hackduke2021-receipts')
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-    key=""
-    if request_json and 'image' in request_json:
-        key = request_json['image']
-    elif request_args and 'image' in request_args:
-        key = request_args['image']
-    else:
-        key = 'bob.png'
-
+    request_json = request.get_json(force=True)
+    key = request_json["image"]
     img = bucket.Object(key).get().get('Body').read()
-    nparray = cv2.imdecode(np.asarray(bytearray(img)), cv2.IMREAD_COLOR)
-    string = pytesseract.image_to_string(nparray)
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=img)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
     ret = ""
-    for k in range(len(string)):
-        if (ord(string[k]) >= 20 and ord(string[k]) <= 126):
-            if (k == 0 or ord(string[k]) != 20 or ord(string[k-1]) != 20):
-                ret += string[k]
-        elif ord(string[k]) == 10:
-            ret += " "
-
-    # print it
+    for text in texts:
+        ret = text.description.replace("\n", " ")
+        break
     return json.dumps({"text": ret})
-
-print(hello_get(None))
