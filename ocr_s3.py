00d36a3c329f2a55810a492960742a982fc140cc
Havish Malladi <havishmalladi@gmail.com>
2021-10-24 03:05:25 -0400

    updated python and java

--- ocr_s3.py
+++
@@ -1,9 +0,0 @@
-import sys
-from flask import escape
-import pytesseract
-import cv2
-import boto3
-import numpy as np
-
-def hello_get(request):
-    return 'Hello World!'
--- python/main.py
+++ python/main.py
@@ -4,6 +4,29 @@ import pytesseract
 import cv2
 import boto3
 import numpy as np
+import os
 
 def hello_get(request):
-    return 'Hello World!'
+    s3 = boto3.resource(
+        service_name='s3',
+        region_name='us-east-1',
+        aws_access_key_id=os.environ.get("ACCESS_KEY"),
+        aws_secret_access_key=os.environ.get("SECRET_KEY")
+    )
+    bucket = s3_resource.Bucket('hackduke2021-receipts')
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+    key=''
+    if request_json and 'image' in request_json:
+        key = request_json['image']
+    elif request_args and 'image' in request_args:
+        key = request_args['image']
+    else:
+        key = 'bob.png'
+
+    img = bucket.Object(key).get().get('Body').read()
+    cv_image = cv2.imread(img)
+    string = pytesseract.image_to_string(cv_image)
+
+    # print it
+    print(string)
