d6e2dc702d7b34fab6206b1e5acc9525deb4499e
mkwak <minjunkwak@gmail.com>
2021-10-23 20:08:49 -0400

    ocr

--- Nodejs/src/app.js
+++ Nodejs/src/app.js
@@ -1,11 +1,26 @@
 const express = require('express')
-
 const app = express()
+const spawn = require('child_process').spawn
 
-app.use(express.json())
+app.get('/text', (req, res) => {
 
-app.get('/', (req, res) => {
-    res.send('whatsup')
+})
+
+// get image from edison and run python script 
+// get list of words, then iterate through and check
+// whether it is in the database
+// if it is, use the def in the database
+// if not, run havishs code to get the web scraped definition
+app.get('/image', (req, res) => {
+    const pythonProcess = spawn('python', ["../../ocr.py", req.body]);
+    pythonProcess.stdout.on('data', (data) => {
+        // Do something with the data returned from python script
+        for (word of data) {
+            // check if its in the database
+            console.log(word)
+        }
+        res.send(data)
+    });
 })
 
 module.exports = app
