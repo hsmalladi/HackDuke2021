7ad503b66137bd44381bfdb27950b37ad437ea4d
mkwak <minjunkwak@gmail.com>
2021-10-23 20:38:09 -0400

    text

--- Nodejs/src/app.js
+++ Nodejs/src/app.js
@@ -3,7 +3,12 @@ const app = express()
 const spawn = require('child_process').spawn
 
 app.get('/text', (req, res) => {
-
+    const words = req.body
+    console.log(words)
+    for (let word of words) {
+        // run java
+    }
+    res.send(words)
 })
 
 // get image from edison and run python script 
@@ -15,7 +20,7 @@ app.get('/image', (req, res) => {
     const pythonProcess = spawn('python', ["../../ocr.py", req.body]);
     pythonProcess.stdout.on('data', (data) => {
         // Do something with the data returned from python script
-        for (word of data) {
+        for (let word of data) {
             // check if its in the database
             console.log(word)
         }
