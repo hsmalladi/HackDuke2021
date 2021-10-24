f016540dfd79c69ce4271d849c4fed56e7f81415
Havish Malladi <havishmalladi@gmail.com>
2021-10-24 05:39:11 -0400

    change to main

--- ocr/main.py
+++ ocr/main.py
@@ -11,13 +11,13 @@ def hello_get(request):
     s3 = boto3.resource(
         service_name='s3',
         region_name='us-east-1',
-        aws_access_key_id=os.environ.get("ACCESS_KEY"),
-        aws_secret_access_key=os.environ.get("SECRET_KEY")
+        aws_access_key_id=os.getenv("ACCESS_KEY"),
+        aws_secret_access_key=os.getenv("SECRET_KEY")
     )
-    bucket = s3_resource.Bucket('hackduke2021-receipts')
+    bucket = s3.Bucket('hackduke2021-receipts')
     request_json = request.get_json(silent=True)
     request_args = request.args
-    key=''
+    key=""
     if request_json and 'image' in request_json:
         key = request_json['image']
     elif request_args and 'image' in request_args:
@@ -26,8 +26,17 @@ def hello_get(request):
         key = 'bob.png'
 
     img = bucket.Object(key).get().get('Body').read()
-    cv_image = cv2.imread(img)
-    string = pytesseract.image_to_string(cv_image)
+    nparray = cv2.imdecode(np.asarray(bytearray(img)), cv2.IMREAD_COLOR)
+    string = pytesseract.image_to_string(nparray)
+    ret = ""
+    for k in range(len(string)):
+        if (ord(string[k]) >= 20 and ord(string[k]) <= 126):
+            if (k == 0 or ord(string[k]) != 20 or ord(string[k-1]) != 20):
+                ret += string[k]
+        elif ord(string[k]) == 10:
+            ret += " "
 
     # print it
     return json.dumps({"text": ret})
+
+print(hello_get(None))
